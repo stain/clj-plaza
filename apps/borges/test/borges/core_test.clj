7184799d6f63d0536fec56cca50e7ae45e375155
Daniel E. Renfer <duck@kronkltd.net>
2012-02-05 16:09:46 -0500

    remove apps

--- apps/borges/test/borges/core_test.clj
+++
@@ -1,3 +0,0 @@
-(ns borges.core-test
-  (:use [borges.core] :reload-all)
-  (:use [clojure.test]))
--- apps/scrummy-clone/server/test/scrummy_clone/core_test.clj
+++
@@ -1,6 +0,0 @@
-(ns scrummy-clone.core-test
-  (:use [scrummy-clone.core] :reload-all)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false))
