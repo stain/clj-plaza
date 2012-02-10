8c7fa131430047c38603d0081383a3ca7b6e6ef2
Daniel E. Renfer <duck@kronkltd.net>
2012-02-09 22:38:16 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -1,26 +1,19 @@
-(defproject plaza "0.0.5-SNAPSHOT"
+(defproject net.kronkltd/plaza "0.0.6-SNAPSHOT"
   :description "Plaza framework for semantic distributed applications"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 [com.hp.hpl.jena/jena "2.6.2"]
-                 [com.hp.hpl.jena/arq "2.8.3"]
-                 [net.rootdev/java-rdfa "0.3"]
-                 [compojure "0.4.1"]
-                 [hiccup "0.2.6"]
-                 [ring/ring-jetty-adapter "0.2.5"]
-                 [nu.validator.htmlparser/htmlparser "1.2.0"]
-                 [com.franz/openrdf-sesame-onejar "2.2"]
-                 [commons-pool "1.5.4"]
-                 [org.jboss.netty/netty       "3.2.0.BETA1"]
-                 [com.rabbitmq/amqp-client "1.7.2"]
-                 [log4j/log4j "1.2.14"]]
-  :repositories { "jboss" "http://repository.jboss.org/nexus/content/groups/public/" }
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]
-                     [clojure-http-client "1.1.0-SNAPSHOT"]
-                     [autodoc "0.7.1"]
-                     [lein-clojars         "0.5.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.0"]
+                 [org.clojure/tools.logging "0.1.2"]
+                 [com.hp.hpl.jena/jena "2.6.4"]
+                 [com.hp.hpl.jena/arq "2.8.8"]
+                 [net.rootdev/java-rdfa "0.4.2-RC2"]
+                 [log4j "1.2.14"]]
+  :repositories {"jboss"
+                 "http://repository.jboss.org/nexus/content/groups/public/" }
+  :exclusions [org.clojure/contrib
+               org.clojure/clojure-contrib]
   :main     plaza.core
-  :autodoc {:name "clj-plaza", :page-title "clj-plaza distributed semantic systems library"
+  :autodoc {:name "clj-plaza",
+            :page-title "clj-plaza distributed semantic systems library"
             :author "Antonio Garrote <antoniogarrote@gmail.com> <agarrote@usal.es>"
             :copyright "2010 (c) Antonio Garrote, under the MIT license"
             :web-home "http://antoniogarrote.github.com/clj-plaza/api"})
